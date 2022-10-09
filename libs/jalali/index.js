22007e3be3c92472d9cd6c6a8acbd7812b6867a0
dev-shayan <smnikzad91@gmail.com>
2022-10-09 08:31:00 +0000

    سیرسیر

--- libs/jalali/index.js
+++ libs/jalali/index.js
@@ -2,13 +2,13 @@ const jalali = require('jalali-moment');
 const moment = require('moment')
 
 exports.persianDate = () => {
-    year   = jalali().locale('fa').toArray()[0];
-    month  = jalali().locale('fa').toArray()[1] + 1;
-    day    = jalali().locale('fa').toArray()[2];
-    hour   = jalali().locale('fa').toArray()[3];
-    minute = jalali().locale('fa').toArray()[4];
-    second = jalali().locale('fa').toArray()[5];
-    date = `${year}/${month}/${day} ${hour}:${minute}:${second}`;  
+    let year   = jalali().locale('fa').toArray()[0];
+    let month  = jalali().locale('fa').toArray()[1] + 1;
+    let day    = jalali().locale('fa').toArray()[2];
+    let hour   = jalali().locale('fa').toArray()[3];
+    let minute = jalali().locale('fa').toArray()[4];
+    let second = jalali().locale('fa').toArray()[5];
+    let date = `${year}/${month}/${day} ${hour}:${minute}:${second}`;  
     return {date, year, month, day, hour, minute, second}   
 }
 
